573baddd25b4a3ce9ec36b10e68f6a2f8c7f6c0a
Fran Gaspar <nectryk@gmail.com>
2025-06-09 19:45:44 +0200

    added client call to exchange api in blazor

---
+++ ExchangeClient/Program.cs
@@ -0,0 +1,36 @@
+using ExchangeClient.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddHttpClient();
+builder.Services.AddHttpClient("alpha-vantage", c =>
+{
+    c.BaseAddress = new Uri(builder.Configuration.GetValue<string>("ExchangeAPI"));
+});
+
+builder.Configuration.GetSection("AlphaVantageApiKey");
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
---
+++ ExchangeLibrary/Models/ExchangeRateModel.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ExchangeLibrary.Models;
+
+public class ExchangeRateModel
+{
+    [JsonPropertyName("Realtime Currency Exchange Rate")]
+    public RealtimeCurrencyExchangeRate RealtimeCurrencyExchangeRate { get; set; }
+}
+
+public class RealtimeCurrencyExchangeRate
+{
+    [JsonPropertyName("1. From_Currency Code")]
+    public string FromCurrencyCode { get; set; }
+
+    [JsonPropertyName("2. From_Currency Name")]
+    public string FromCurrencyName { get; set; }
+
+    [JsonPropertyName("3. To_Currency Code")]
+    public string ToCurrencyCode { get; set; }
+
+    [JsonPropertyName("4. To_Currency Name")]
+    public string ToCurrencyName { get; set; }
+
+    [JsonPropertyName("5. Exchange Rate")]
+    public string ExchangeRate { get; set; }
+
+    [JsonPropertyName("6. Last Refreshed")]
+    public string LastRefreshed { get; set; }
+
+    [JsonPropertyName("7. Time Zone")]
+    public string TimeZone { get; set; }
+}
